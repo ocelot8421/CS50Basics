55996c66f91cba36f630ca5ec936152dcc6578a7
ocelot8421 <8ocelot@gmail.com>
2023-09-15 16:28:12 +0200

    luncher and debugger setting

---
+++ week10/main.lua
@@ -0,0 +1,36 @@
+--debugger
+if arg[2] == "debug" then
+    require("lldebugger").start()
+end
+
+
+
+--function love.draw()
+--    love.graphics.print("Hello World!", 100, 100)
+--end
+
+
+example = function ()
+    print("Hello World!")
+end
+
+
+function example2()
+    print("Hello World!")
+end
+
+
+example()
+example2()
+
+
+--errorhandler
+local love_errorhandler = love.errhand
+
+function love.errorhandler(msg)
+    if lldebugger then
+        error(msg, 2)
+    else
+        return love_errorhandler(msg)
+    end
+end
