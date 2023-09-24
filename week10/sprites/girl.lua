a7d8f1d25dfa288ed3fa62e23e0b281596f07c45
ocelot8421 <8ocelot@gmail.com>
2023-09-24 19:22:38 +0200

    oscillation vertical - girl (delta y)

--- week10/sprites/girl.lua
+++ week10/sprites/girl.lua
@@ -2,17 +2,19 @@ require "sprites.sprite"
 
 Girl = Sprite:extend()
 
-function Girl:new(path, x, y, speed, scale)
+function Girl:new(path, x, y, amplitudo, scale, leashLength)
     Girl.super.new(self, path, x, y, scale)
-    self.speed = speed
-    self.amplitudo = 100
+    self.leashLength = leashLength
+    self.amplitudo = amplitudo
     self.time = 0
     self.posAngle = 0
 end
 
 function Girl:update(dt, dogX, dogY)
     if self.posAngle < 3000 then
-        self.x = self.x - self.amplitudo * math.pow(2.7182818284, -0.1 * dt * self.posAngle) * math.cos(math.rad(self.posAngle)) * dt
+        deltaX = self.amplitudo * math.pow(2.7182818284, -0.1 * dt * self.posAngle) * math.cos(math.rad(self.posAngle)) * dt
+        self.x = self.x + deltaX -- original position
+        self.y = self.y + leashLength - math.sqrt(math.pow(self.leashLength, 2) - math.pow(deltaX, 2))
         self.posAngle = self.posAngle + 1
         print(self.posAngle)
         print(math.pow(2.7182818284, -0.3 * dt * self.posAngle))
